fb27dc3cd6dfc342cda370fd73f40c9a0f0474f0
RostyslavR <rrmail@ukr.net>
2023-06-23 20:16:19 +0300

    path?

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const SERVER_DIR = __dirname;
 const TMP_DIR = path.join(__dirname, "tmp");
 // const FILE_DIR = path.join(__dirname, "public", "files");
-const FILE_DIR = path.join(__dirname);
+const FILE_DIR = path.join(__dirname, "public");
 
 module.exports = {
   SERVER_DIR,
